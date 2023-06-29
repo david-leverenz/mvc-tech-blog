2783ae9a985317dd47cf33c383f4e62021ba4fbd
david-leverenz <david.leverenz@gmail.com>
2023-06-29 17:12:20 -0500

    blog post route working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes')
-const blogRoutes = require('')
+const blogRoutes = require('./blogRoutes')
 // const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
 router.use('/comments', commentRoutes);
+router.use('/blog', blogRoutes);
 // router.use('/projects', projectRoutes);
 
 module.exports = router;
