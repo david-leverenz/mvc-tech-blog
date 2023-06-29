7037dc36be6539a42228e5048cdcd9a451054172
david-leverenz <david.leverenz@gmail.com>
2023-06-29 17:21:13 -0500

    blog delete done

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const { User, Blog_data, Comments } = require('../../models');
+const { Blog_data } = require('../../models');
 
-//I want this to create a new post but couldn't pull the brain power to reason through it.
 router.post('/', async (req, res) => {
     try {
       const newBlog = await Blog_data.create({
         ...req.body,
-        user_id: req.session.user_id, // user id is coming from the user session, user_id from the UserRoutes
+        user_id: req.session.user_id,
       });
   
       res.status(200).json(newBlog);
@@ -14,26 +13,25 @@ router.post('/', async (req, res) => {
       res.status(400).json(err);
     }
   });
-// router.get('/:id', async (req, res) => {
-//     // find one category by its `id` value
-//     // be sure to include its associated Products
-//     try {
-//         const blogData = await Blog_data.findByPk(req.params.id, {
-//             include: [{ model: User }],
-//         });
 
-//         if (!blogData) {
-//             res.status(404).json({ message: 'No blog found with that id!' });
-//             return;
-//         }
-
-//         //   const showBlogs = blogData.map(blog => blog.get({ plain: true }));
-
-//         res.status(200).json(blogData);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+  router.delete('/:id', async (req, res) => {
+    try {
+      const oneBlog = await Blog_data.destroy({
+        where: {
+          id: req.params.id
+          },
+      });
+  
+      if (!oneBlog) {
+        res.status(404).json({ message: 'No blog found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(`blog with id of ${req.params.id} deleted`);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 // /* <a href="/{{blog_id}}"> */</a>
 
--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -30,7 +30,7 @@ router.delete('/:id', async (req, res) => {
         return;
       }
   
-      res.status(200).json(`id ${req.params.id} deleted`);
+      res.status(200).json(`comment with id of ${req.params.id} deleted`);
     } catch (err) {
       res.status(500).json(err);
     }
