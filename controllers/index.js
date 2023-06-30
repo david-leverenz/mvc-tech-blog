f6f686a9af7473ea9412271674977e36acd6176e
david-leverenz <david.leverenz@gmail.com>
2023-06-30 16:38:21 -0500

    starting to link front to back

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoute = require('./homeRoutes')
+const viewRoutes = require('./viewRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoute);
---
+++ controllers/viewRoutes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+// const { User, Messages, Room} = require('../models');
+// const auth = require('./utils/auth')
+
+// router.get('/', async (req, res) => {
+//     try {
+//         const roomData = await Room.findAll({
+//             include: [{
+//                 model: User,
+//                 through: Messages,
+//             }]
+//         });
+//         const newRoomData = roomData.map(room => room.get ({ plain:true }));
+//         console.log(newRoomData);
+//         res.render('chat', {newRoomData});
+//     } catch (error) {
+//         res.status(500).json(error)
+//     };
+// });
+// router.get('/',(req,res)=>{
+//     res.redirect('/chat')
+// })
+// router.get('/chat', (req, res) => {
+//     res.render('chat')
+// })
+// // com
+router.get('/login', async (req, res) => {
+    try {
+
+        res.render('login');
+    } catch (error) {
+        res.status(500).json(error)
+    };
+});
+
+router.get('/signup', async (req,res) => {
+    try {
+        res.render('signup');
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+// router.get('/profile', async (req, res) => {
+//     try {
+
+//         res.render('profile');
+//     } catch (error) {
+//         res.status(500).json(error)
+//     };
+// });
+
+
+
+module.exports = router;
